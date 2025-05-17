fcbebba341bf0bc380d455e7a131fe4986874bfa
Jared Halpern <jared@jaredhalpern.com>
2025-05-16 22:55:56 -0400

    fixed asset loading

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     GameEngine engine("Simple Game", 800, 600);
 
-    const char *imagePath = "../assets/player.png";
+    const char *imagePath = "./assets/player.png";
     // GameObject *player = new GameObject(engine.renderer, imagePath, 100, 100, 50, 50);
 
     SDL_Surface *surface = IMG_Load(imagePath);
@@ -13,7 +13,7 @@ int main()
         SDL_Log("Failed to load image %s: %s", imagePath, IMG_GetError());
     } else {
         SDL_Log("Loaded image %s", imagePath);
-        GameObject *player = new GameObject(engine.renderer, imagePath, 100, 100, 50, 50);
+        GameObject *player = new GameObject(engine.renderer, imagePath, 100, 100, 250, 250);
         SDL_FreeSurface(surface);
         engine.addObject(player);
     }
